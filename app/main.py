daa440ac4d6e2c8a658d0a05ebbc46e4343f9c91
Admin <SOVAinhere@yandex.ru>
2026-01-22 23:37:50 +0300

    /

--- app/main.py
+++ app/main.py
@@ -2,8 +2,8 @@ from fastapi import FastAPI, HTTPException, Depends
 from sqlalchemy.orm import Session
 from passlib.context import CryptContext
 
-from database import engine, SessionLocal
-from models import Base, User
+from app.database import engine, SessionLocal
+from app.models import Base, User
 
 app = FastAPI()
 
@@ -22,9 +22,31 @@ pwd_context = CryptContext(
 def hash_password(password: str) -> str:
     return pwd_context.hash(password)
 
+
 def get_db():
     db = SessionLocal()
     try:
         yield db
     finally:
         db.close()
+
+
+@app.post("/signup")
+def signup(
+        email: str,
+        password: str,
+        db: Session = Depends(get_db)
+):
+    existing_user = db.query(User).filter(User.email == email).first()
+    if existing_user:
+        raise HTTPException(status_code=400, detail="Email already registered")
+
+    user = User(email=email, password_hash=hash_password(password))
+
+    db.add(user)
+    db.commit()
+    db.refresh(user)
+    return {
+        "id": user.id,
+        "email": user.email,
+    }
--- app/models.py
+++ app/models.py
@@ -1,11 +1,11 @@
 from sqlalchemy import Column, Integer, String, Boolean
-from database import Base
-
+from app.database import Base
 
 class User(Base):
-    __tablename__ = 'users'
-    id = Column(Integer, primary_key=True)
-    email = Column(String, unique=True, nullable=False)
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True, nullable=False)
     password_hash = Column(String, nullable=False)
     is_active = Column(Boolean, default=True)
-    role = Column(String, nullable=False)
+    role = Column(String, default="user")
