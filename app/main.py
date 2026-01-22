e8b7ef09cdf26adb3ba4f31c86a842e08d183b2a
Admin <SOVAinhere@yandex.ru>
2026-01-22 21:54:47 +0300

    /

--- app/main.py
+++ app/main.py
@@ -1,4 +1,9 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, Depends
+from sqlalchemy.orm import Session
+from passlib.context import CryptContext
+
+from database import engine, SessionLocal
+from models import Base, User
 
 app = FastAPI()
 
@@ -7,3 +12,12 @@ app = FastAPI()
 def root():
     return {"status": "ok"}
 
+
+pwd_context = CryptContext(
+    schemes=["bcrypt"],
+    deprecated="auto"
+)
+
+
+def hash_password(password: str) -> str:
+    return pwd_context.hash(password)
--- app/models.py
+++ app/models.py
@@ -2,7 +2,7 @@ from sqlalchemy import Column, Integer, String, Boolean
 from database import Base
 
 
-class Users(Base):
+class User(Base):
     __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
     email = Column(String, unique=True, nullable=False)
